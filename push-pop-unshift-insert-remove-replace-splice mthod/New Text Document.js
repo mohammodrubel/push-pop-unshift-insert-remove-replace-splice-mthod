2b787e3c34612d6ab377820c10cc6fbec3528756
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-06 21:12:19 +0600

    Add files via upload

---
+++ push-pop-unshift-insert-remove-replace-splice mthod/New Text Document.js
@@ -0,0 +1,30 @@
+var arr = [123,456,789,12,34,56,78,9]
+
+
+// push method
+arr.push(3456)
+console.log(arr)
+
+// unshift method 
+arr.unshift(34456)
+console.log(arr)
+
+// pop method 
+arr.pop()
+console.log(arr)
+
+// // insert array 
+arr.splice(3,0,2345)
+arr.splice (5,0,4556,3445)
+
+console.log(arr)
+//array remove 
+arr.splice(3,1)
+console.log(arr)
+arr.splice(3,3)
+console.log(arr)
+
+// replace or update array data 
+arr[4]=23456345
+
+console.log(arr)
